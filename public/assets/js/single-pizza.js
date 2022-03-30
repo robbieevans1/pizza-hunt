822f334750b8b054b335126f595506aa67794b53
robcabobb <robcabobb@gmail.com>
2022-03-30 17:35:15 -0400

    added code to public single pizza js file to handle new replies

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -151,27 +151,50 @@ function handleNewCommentSubmit(event) {
 
 
 function handleNewReplySubmit(event) {
-	event.preventDefault();
+  event.preventDefault();
 
-	if (!event.target.matches(".reply-form")) {
-		return false;
-	}
+  if (!event.target.matches('.reply-form')) {
+    return false;
+  }
 
-	const commentId = event.target.getAttribute("data-commentid");
+  const commentId = event.target.getAttribute('data-commentid');
 
-	const writtenBy = event.target.querySelector("[name=reply-name]").value;
-	const replyBody = event.target.querySelector("[name=reply]").value;
+  const writtenBy = event.target.querySelector('[name=reply-name]').value;
+  const replyBody = event.target.querySelector('[name=reply]').value;
 
-	if (!replyBody || !writtenBy) {
-		return false;
-	}
+  if (!replyBody || !writtenBy) {
+    return false;
+  }
 
-	const formData = { writtenBy, replyBody };
+  const formData = { writtenBy, replyBody };
+
+  fetch(`/api/comments/${pizzaId}/${commentId}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      response.json();
+    })
+    .then(commentResponse => {
+      console.log(commentResponse);
+      location.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
-$backBtn.addEventListener("click", function () {
-	window.history.back();
+$backBtn.addEventListener('click', function() {
+  window.history.back();
 });
+
 
 $newCommentForm.addEventListener("submit", handleNewCommentSubmit);
 $commentSection.addEventListener("submit", handleNewReplySubmit);
